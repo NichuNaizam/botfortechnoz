d33ebe4b4dbc24207853afd1d881bce44a01d67c
NichuNaizam <naizamaliz141@gmail.com>
2020-07-09 14:27:01 +0530

    commitment

--- index.js
+++ index.js
@@ -1,97 +1,76 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const config = require('./config.json');
+const token = process.env.TOKEN;
 
 const mineflayer = require('mineflayer');
 
+
+const scaffold = require("mineflayer-scaffold")(mineflayer);
+const navigate = require("mineflayer-navigate")(mineflayer);
+
 const prefix = 't!';
 
-client.on('ready' ,async=> {
-    console.log("Bot Ready");
+client.on('ready', async => {
+  console.log("Bot Ready");
 });
 
-client.on('message',message => {
+client.on('message', message => {
 
-    if(message.author.bot)return;
-    if(message.channel.id == "728901827277619240"){
-        message.delete();
-        bot.chat(message.author.username + " > " + `${message}`);
-    }
+  if (message.author.bot) return;
+  if (message.channel.id == "728901827277619240") {
+    message.delete();
+    bot.chat(message.author.username + " > " + `${message}`);
+  }
 
-    if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    const args = message.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();
 
-    switch(command){
-        case 'ping':
-            message.reply("Pong");
-            message.react('🏓');
-        break;
-    }
+  switch (command) {
+    case 'ping':
+      message.reply("Pong");
+      message.react('🏓');
+      break;
+  }
 });
 
-client.login(config.TOKEN);
+client.login(token);
 
 // aternos bot code!
 
 const bot = mineflayer.createBot({
-    host: "technodreamcity.aternos.me",
-    username: "TechnozBot",
-    version: "1.15.2" 
+  host: "technodreamcity.aternos.me",
+  username: "TechnozBot",
+  version: false,
 });
 
-bot.on("move", function() {
-    //triggers when the bot moves
-  
-    bot.setControlState("jump", true); //continuously jumps
-    setTimeout(() => {
-      //sets a delay
-      bot.setControlState("jump", false); //stops jumping
-    }, 1000); //delay time
-  
-    setTimeout(() => {
-      //sets a delay
-      bot.setControlState("forward", true); //continuously walks forward
-      setTimeout(() => {
-        //sets a delay
-        bot.setControlState("forward", false); //stops walking forward
-      }, 500); //delay time
-    }, 1000); //delay time
-  
-    setTimeout(() => {
-      //sets a delay
-      bot.setControlState("back", true); //continuously walks backwards
-      setTimeout(() => {
-        //sets a delay
-        bot.setControlState("back", false); //stops walking backwards
-      }, 500); //delay time
-    }, 2000); //delay time
-  
-    setTimeout(() => {
-      //sets a delay
-      bot.setControlState("right", true); //continuously walks right
-      setTimeout(() => {
-        //sets a delay
-        bot.setControlState("right", false); //stops walking right
-      }, 2000); //delay time
-    }, 500); //delay time
-  
-    setTimeout(() => {
-      //sets a delay
-      bot.setControlState("left", true); //continuously walks lefz
-      setTimeout(() => {
-        //sets a delay
-        bot.setControlState("left", false); //stops walking left
-      }, 2000); //delay time
-    }, 500); //delay time
-  });
+navigate(bot);
+scaffold(bot);
 
-bot.on('message',botmessage=>{
-    client.channels.cache.get("728901827277619240").send(`${botmessage}`);
-});
+bot.on('chat', function (username, botmessage) {
 
-bot.on('login', () =>{
-    bot.chat("hello im Techozbot, create for technoz by NichuNaizam")
+  if (botmessage == "t!come") {
+    var target = bot.players[username].entity;
+    bot.chat("comming");
+    bot.scaffold.to(target.position, function (err) {
+      if (err) {
+        bot.chat("didn't make it: " + err.code);
+      } else {
+        bot.chat("made it!");
+      }
+    });
+    if (botmessage == bot.username) {
+      bot.chat("I was created for Technoz");
+      bot.chat("NichuNaizam created me");
+      bot.chat("im just like a discord bot ");
+      bot.chat("u guys can send me command");
+      bot.chat("my prefix is t!");
+      bot.chat("checkout my help command t!help (currently not ready)");
+    }
+  }
+  let outputchannel = client.channels.cache.get("728901827277619240");
+  if (!outputchannel) return;
+  outputchannel.send(`${username}` + " > " + `${botmessage}`);
 });
